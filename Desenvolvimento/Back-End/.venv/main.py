7ddcd0c6549bd19be1095f3a6276b06228bf5607
victorcmoraes <victor.cmoraes@ufrpe.br>
2023-12-07 19:36:37 -0300

    in API
    API to req1 e req2

---
+++ Desenvolvimento/Back-End/.venv/main.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+import json
+import os
+
+app = Flask(__name__)
+db_file = 'tasks.txt'
+
+def load_db():
+    if os.path.exists(db_file):
+        with open(db_file, 'r') as f:
+            tasks = json.load(f)
+    else:
+        tasks = {}
+    return tasks
+
+def save_db(tasks):
+    with open(db_file, 'w') as f:
+        json.dump(tasks, f)
+
+@app.route('/', methods=['GET'])
+def teste():
+    return jsonify({'message' : 'funciona'})
+
+@app.route('/task', methods=['POST'])
+def criar_task():
+    tasks = load_db()
+    data = request.get_json()
+    id = str(max(map(int, tasks.keys())) + 1) if tasks else '1'
+    tasks[id] = {'title': data['title'], 'description': data['description'], 'deadline': data['deadline'], 'difficulty': data['difficulty']}
+    save_db(tasks)
+    return jsonify({'message': 'Tarefa criada com sucesso!', 'id': id})
+
+@app.route('/task/<id>', methods=['PUT'])
+def update_task(id):
+    tasks = load_db()
+    data = request.get_json()
+    if id in tasks:
+        tasks[id] = {'title': data['title'], 'description': data['description'], 'deadline': data['deadline'], 'difficulty': data['difficulty']}
+        save_db(tasks)
+        return jsonify({'message': 'Tarefa atualizada com sucesso!'})
+    else:
+        return jsonify({'message': 'Tarefa não encontrada!'}), 404
+
+@app.route('/task/<id>', methods=['DELETE'])
+def delete_task(id):
+    tasks = load_db()
+    if id in tasks:
+        del tasks[id]
+        save_db(tasks)
+        return jsonify({'message': 'Tarefa excluída com sucesso!'})
+    else:
+        return jsonify({'message': 'Tarefa não encontrada!'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
