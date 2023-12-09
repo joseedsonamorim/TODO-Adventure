b162dd8d38c80807e10f000747e527a8e1d01239
Vinicius Albuquerque (Apolo) <vinicius.cius@gmail.com>
2023-12-09 08:06:45 -0300

    editando o dialog para receber dados de missões

--- Desenvolvimento/Front-End/to-do-adventure/src/app/dialog-form-mission/dialog-form-mission.component.ts
+++ Desenvolvimento/Front-End/to-do-adventure/src/app/dialog-form-mission/dialog-form-mission.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import Mission from '../shared/models/mission-model';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-form-mission',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-form-mission.component.scss']
 })
 export class DialogFormMissionComponent implements OnInit {
+  mission?: Mission;
+  selectedDifficulty: string = "Fácil";
+  labelButton: string = "Criar";
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    this.mission = data;
+    if (this.mission) {
+      this.selectedDifficulty = this.mission.difficulty;
+      this.labelButton = "Editar"
+    }
+   }
 
   ngOnInit(): void {
-  }
-
+    }
 }
