9f96595f00e933587958a45bfcbf3d01ada48e1c
Vinicius Albuquerque (Apolo) <vinicius.cius@gmail.com>
2023-12-09 11:44:57 -0300

    resolvendo input de data

--- Desenvolvimento/Front-End/to-do-adventure/src/app/dialog-form-mission/dialog-form-mission.component.ts
+++ Desenvolvimento/Front-End/to-do-adventure/src/app/dialog-form-mission/dialog-form-mission.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import Mission from '../shared/models/mission-model';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-form-mission',
@@ -10,16 +11,23 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class DialogFormMissionComponent implements OnInit {
   mission?: Mission;
   selectedDifficulty: string = "Fácil";
-  labelButton: string = "Criar";
+  label: string = "Criar";
+  deadline?: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.mission = data;
     if (this.mission) {
       this.selectedDifficulty = this.mission.difficulty;
-      this.labelButton = "Editar"
+      this.label = "Editar";
+      this.setDeadline(this.mission.deadline);
     }
    }
 
   ngOnInit(): void {
     }
+
+  setDeadline(data: string) {
+    const array_data = data.split('/');
+    this.deadline = [array_data[2], array_data[1], array_data[0]].join('-');
+  }
 }
