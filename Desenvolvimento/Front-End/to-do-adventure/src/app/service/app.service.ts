e9ac508831cdf00287bf0a1233562e26a652e39e
Vinicius Albuquerque (Apolo) <vinicius.cius@gmail.com>
2024-02-17 04:04:20 -0300

    adaptação do front com relação as mudanças no back

--- Desenvolvimento/Front-End/to-do-adventure/src/app/service/app.service.ts
+++ Desenvolvimento/Front-End/to-do-adventure/src/app/service/app.service.ts
@@ -7,51 +7,50 @@ import { Observable } from 'rxjs';
 })
 export class AppService {
 
-  private urlTask = 'http://127.0.0.1:5000/task';  // Substitua pela URL da sua API
-  private urlJourney = 'http://127.0.0.1:5000/jornadas';  // Substitua pela URL da sua API
+  private urlJourney = 'http://127.0.0.1:5000/journeys';
 
   constructor(private http: HttpClient) { }
 
-  getMissions(): Observable<any> {
-    return this.http.get<any>(this.urlTask);
-  }
-
-  createTask(data: any): Observable<any> {
-    return this.http.post(this.urlTask, data);
-  }
-
-  updateTask(id?: number, data?: any): Observable<any> {
-    const url = `${this.urlTask}/${id}`;
-    return this.http.put(url, data);
-  }
-
-  deleteTask(id?: number): Observable<any> {
-    const url = `${this.urlTask}/${id}`;
-    return this.http.delete(url);
-  }
-
-  //Rotas jornada do héroi
-
-  getJounerys(): Observable<any> {
+  getJourneys(): Observable<any> {
     return this.http.get<any>(this.urlJourney);
   }
-
-  getJourney(jorney?: string): Observable<any> {
-    const url = `${this.urlJourney}/${jorney}`;
-    return this.http.get<any>(url);
-  }
-
+  
   createJourney(data: any): Observable<any> {
     return this.http.post(this.urlJourney, data);
   }
 
-  updateJourney(id?: number, data?: any): Observable<any> {
-    const url = `${this.urlJourney}/${id}`;
+  updateJourney(journeyId: number, data: any): Observable<any> {
+    const url = `${this.urlJourney}/${journeyId}`;
     return this.http.put(url, data);
   }
 
-  deleteJourney(id?: number): Observable<any> {
-    const url = `${this.urlJourney}/${id}`;
+  deleteJourney(journeyId: number): Observable<any> {
+    const url = `${this.urlJourney}/${journeyId}`;
     return this.http.delete(url);
+  }
+
+  createMission(journeyId: number, data: any): Observable<any> {
+    const url = `${this.urlJourney}/${journeyId}/missions`;
+    return this.http.post(url, data);
+  }
+
+  updateMission(journeyId: number, missionId: number, data: any): Observable<any> {
+    const url = `${this.urlJourney}/${journeyId}/missions/${missionId}`;
+    return this.http.put(url, data);
+  }
+
+  deleteMission(journeyId: number, missionId: number): Observable<any> {
+    const url = `${this.urlJourney}/${journeyId}/missions/${missionId}`;
+    return this.http.delete(url);
+  }
+
+  getSettedJourney(): Observable<any> {
+    const url = 'http://127.0.0.1:5000/setted_journey';
+    return this.http.get<any>(url);
+  }
+
+  setSettedJourney(journeyId: number): Observable<any> {
+    const url = `http://127.0.0.1:5000/setted_journey/${journeyId}`;
+    return this.http.put(url, {});
   }
 }
